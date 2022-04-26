41c18029ddb8eee238562c6d5fde603d7234171c
ET45 <emre.tugutlu@gmail.com>
2022-04-26 13:42:48 +0300

    ...

--- index.js
+++
@@ -1,4 +0,0 @@
-var block = document.getElementById("block");
-var hole = document.getElementById("hole");
-
-hole.addEventListener("animationiteration", () => {});
---
+++ script.js
@@ -0,0 +1,6 @@
+const block = document.getElementById("block");
+const hole = document.getElementById("hole");
+
+hole.addEventListener("animationiteration", () => {
+  const random = Math.random();
+});
