bcabfc43837630d07c5fe4194268da944e8be2f8
ET45 <emre.tugutlu@gmail.com>
2022-04-26 14:19:40 +0300

    gravity added

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const block = document.getElementById("block");
 const hole = document.getElementById("hole");
+const character = document.getElementById("character");
 
 hole.addEventListener("animationiteration", () => {
   const random = -Math.random() * 3;
   const top = random * 100 + 150;
   hole.style.top = -top + "px";
 });
+setInterval(function () {
+  const characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  character.style.top = characterTop + 3 + "px";
+}, 10);
