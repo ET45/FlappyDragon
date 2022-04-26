cbfe62bec5291d852773f03d89d2499be0fe298e
ET45 <emre.tugutlu@gmail.com>
2022-04-26 14:53:15 +0300

    onclick not working

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ setInterval(function () {
   const characterTop = parseInt(
     window.getComputedStyle(character).getPropertyValue("top")
   );
-  if (jumping === 0) {
+  if (jumping == 0) {
     character.style.top = characterTop + 3 + "px";
   }
 }, 10);
 
-const jump = () => {
-  const jumping = 1;
+function jump() {
+  jumping = 1;
   let jumpCount = 0;
   const jumpInterval = setInterval(function () {
     const characterTop = parseInt(
       window.getComputedStyle(character).getPropertyValue("top")
     );
-    if (characterTop > 6) {
+    if (characterTop > 6 && jumpCount < 15) {
       character.style.top = characterTop - 5 + "px";
     }
     if (jumpCount > 20) {
@@ -32,7 +32,6 @@ const jump = () => {
       jumping = 0;
       jumpCount = 0;
     }
-
     jumpCount++;
   }, 10);
-};
+}
