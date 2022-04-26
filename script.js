3927998a742ce318a683f58a7313a00179d02f64
ET45 <emre.tugutlu@gmail.com>
2022-04-26 15:22:14 +0300

    game done with scores

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const block = document.getElementById("block");
 const hole = document.getElementById("hole");
 const character = document.getElementById("character");
-const jumping = 0;
-const counter = 0;
+let jumping = 0;
+let counter = 0;
 
 hole.addEventListener("animationiteration", () => {
   const random = -(Math.random() * 300 + 150);
