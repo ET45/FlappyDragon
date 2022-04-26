dfc863139bf7f050af070237cec67838d0476453
ET45 <emre.tugutlu@gmail.com>
2022-04-26 14:14:05 +0300

    character added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const block = document.getElementById("block");
 const hole = document.getElementById("hole");
 
 hole.addEventListener("animationiteration", () => {
-  const random = -(Math.random() * 300 + 150);
-  hole.style.top = random + "px";
+  const random = -Math.random() * 3;
+  const top = random * 100 + 150;
+  hole.style.top = -top + "px";
 });
