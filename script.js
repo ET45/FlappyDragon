207a00839dc374a46bd1c5c7d989d5ca7efc11d5
ET45 <emre.tugutlu@gmail.com>
2022-04-26 13:53:38 +0300

    fixes on css

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const block = document.getElementById("block");
 const hole = document.getElementById("hole");
 
 hole.addEventListener("animationiteration", () => {
-  const random = Math.random();
+  const random = -(Math.random() * 300 + 150);
+  hole.style.top = random + "px";
 });
